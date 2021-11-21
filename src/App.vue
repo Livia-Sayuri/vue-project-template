3277a17bb02c8f1ed55f10691244dc7da48def8c
Lívia Sayuri <69463877+Livia-Sayuri@users.noreply.github.com>
2021-11-21 10:38:47 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+
+  <v-system-bar app>
+      <v-spacer></v-spacer>
+      <span>v-system-bar</span>
+            <v-spacer></v-spacer>
+            </v-system-bar>
+
+    <v-app-bar app permanent fixed >
+          <v-spacer></v-spacer>
+      <span>v-app-bar</span>
+       <v-spacer></v-spacer>
+    </v-app-bar>
+
+<v-row align="center">
+
+    <v-row justify="space-around">
+      <v-navigation-drawer app permanent clipped class="mt-100">
+      <span>v-navigation-drawer</span>
+    </v-navigation-drawer>
+  </v-row>
+
+    <v-row justify="space-around">
+    <v-content >
+      <span>v-content</span>
+    </v-content>
+    </v-row>
+</v-row>
+
+    <v-footer class="mt-5">
+      <v-spacer></v-spacer>
+      <span>v-footer</span>
+    <v-spacer></v-spacer>
+    </v-footer>
+
+<v-bottom-navigation>
+  <v-spacer></v-spacer>
+  <span>v-bottom-navigation</span>
+  <v-spacer></v-spacer>
+</v-bottom-navigation>
+
+  </v-app>
+</template>
